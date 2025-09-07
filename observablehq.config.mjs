4c33007e265ac82e6b638edb0e5cd7ee81226ec1
Jonah Weissman <jonahrweissman@gmail.com>
2025-09-06 20:06:27 -0400

    another way to disable range requests

--- observablehq.config.mjs
+++ observablehq.config.mjs
@@ -1,7 +1,11 @@
 export default {
   title: "metrohenge",
-  // Root content directory (defaults to "src")
   root: "src",
   // Default output is "dist"; leave as-is for now to follow Observable defaults.
+  duckdb: {
+      filesystem: {
+        forceFullHTTPReads: true
+      }
+  }
 };
 
